6818eb16ca048083af8f15640187a5e09d603a3a
Nikhil Ajay Kadalge <nkadalge@gmail.com>
2022-05-11 21:30:52 -0500

    Time: 101 ms (50.98%), Space: 17.3 MB (80.10%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,24 +1,11 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        maximum = 0
-        minimum = 10000000
-        for i in range(len(intervals)):
-            maximum = max(int(intervals[i][0]),maximum)
-            minimum = min(int(intervals[i][0]),minimum)
+        intervals.sort()
+        
+        for i in range(1,len(intervals)):
+            start_1, end_1 = intervals[i-1]
+            start_2, end_2 = intervals[i]
             
-            maximum = max(int(intervals[i][1]),maximum)
-            minimum = min(int(intervals[i][1]),minimum)
-        
-        arr = [0]*((2*maximum)-minimum+1)
-        
-        
-        for i in range(len(intervals)):
-            arr[intervals[i][0]] = 0
-            arr[intervals[i][1]] = 0
-            for j in range(intervals[i][0],intervals[i][1]+1):
-                if arr[j] == 0:
-                    arr[j] = 1
-                else:
-                    return False
+            if end_1 > start_2: return False
         
         return True
